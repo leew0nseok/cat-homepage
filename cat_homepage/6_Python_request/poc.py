d33d2480db656d1e9baf57bca32881db610f161f
leew0nseok <zzang50aa@gmail.com>
2024-04-25 01:19:07 +0900

    Python Request

---
+++ cat_homepage/6_Python_request/poc.py
@@ -0,0 +1,11 @@
+import requests
+
+url = 'http://localhost:8000/page-babo.php'
+
+cookies = {
+    'user':'babo_bs'
+}
+
+response = requests.get(url, cookies=cookies,allow_redirects=False)
+
+print(response.text)
